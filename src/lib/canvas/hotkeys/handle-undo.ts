716d4bae6db47d1485934d101f75e2b7f59f3d4a
Toaster1123 <prokhorov.rostislav@gmail.com>
2025-02-27 13:47:27 +0300

    refactor(canvas): refacror undo function

--- src/lib/canvas/hotkeys/handle-undo.ts
+++ src/lib/canvas/hotkeys/handle-undo.ts
@@ -7,16 +7,17 @@ export const handleUndo = (
   redoStack: RefObject<string[]>,
   canvas: Canvas,
 ) => {
+  if (history.current.length <= 1) return;
   isSaving.current = true;
 
-  const prevState = JSON.parse(history.current[history.current.length - 1]);
   const currentState = history.current.pop() as string;
   redoStack.current = [...redoStack.current, currentState];
+  const prevState = JSON.parse(history.current[history.current.length - 1]);
 
   if (prevState) {
     canvas.loadFromJSON(prevState).then(() => {
       console.log('prevState', prevState);
-      console.log('canvas after', canvas);
+      console.log('canvas after', canvas._objects);
       canvas.renderAll();
       isSaving.current = false;
     });
--- src/lib/canvas/hotkeys/save-state.ts
+++ src/lib/canvas/hotkeys/save-state.ts
@@ -8,9 +8,10 @@ export const saveState = (
   canvas: Canvas | null,
 ) => {
   if (!canvas || isSaving.current) return;
-  canvas.getObjects().forEach((obj) => {
+  canvas.forEachObject((obj) => {
     obj.toObject = ((toObject) =>
       function (this: FabricObject) {
+        console.log('this', this);
         return {
           ...toObject.call(this),
           selectable: this.selectable,
@@ -18,15 +19,17 @@ export const saveState = (
           name: this.name,
           opacity: this.opacity,
           angle: this.angle,
+          width: this.getScaledWidth() - this.strokeWidth,
+          height: this.getScaledHeight() - this.strokeWidth,
           left: this.left,
           top: this.top,
         };
       })(obj.toObject);
   });
-  console.log('canvas befot ew save', canvas._objects[canvas._objects.length - 1]);
+  console.log('canvas before save last obj', canvas._objects[canvas._objects.length - 1]);
   const currentState = JSON.stringify(canvas.toJSON());
   console.log('history before', history.current);
-  console.log(canvas._objects);
+  console.log('canvas save all objs', canvas._objects);
   if (history.current.length === 0 || history.current[history.current.length - 1] != currentState) {
     history.current = [...history.current, currentState];
     console.log('history after', history.current);
